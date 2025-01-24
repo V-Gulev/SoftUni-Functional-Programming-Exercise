bc22a6dbd52e8e141e96156b36f1973c3cce9e0e
V-Gulev <velizargulev@abv.bg>
2025-01-24 20:14:40 +0200

    SortEvenNumbers_01

--- SoftUni/SortEvenNumbers_01.java
+++ SoftUni/SortEvenNumbers_01.java
@@ -1,5 +1,19 @@
 package SoftUni;
 
-public class SortEvenNumbers_01 {
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
+public class SortEvenNumbers_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> numbers = Arrays.stream(sc.nextLine().split(",\\s+")).map(Integer::parseInt).filter(num -> num % 2 == 0).toList();
+        System.out.println(numbers.toString().replaceAll("\\[", "").replaceAll("]", ""));
+        List<Integer> sortedNumbers = numbers.stream().sorted(Comparator.naturalOrder()).toList();
+        System.out.println(sortedNumbers.toString().replaceAll("\\[", "").replaceAll("]", ""));
+
+
+    }
 }
